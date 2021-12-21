acfdc63b9c99c759b7aa93a1d2decc30ea8c51b3
Ceyda Kamalı <ceydakamali3@gmail.com>
2021-12-21 23:48:26 +0300

    edited articles model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from blog.models import KategoriModel
+from blog.models import KategoriModel, YazilarModel
 
 # Register your models here.
 admin.site.register(KategoriModel)
+
+class YazilarAdmin(admin.ModelAdmin):
+    search_fields=('baslik','icerik')
+    list_display=(
+        'baslik', 'olusturulma_tarihi', 'duzenleme_tarihi'
+    )
+admin.site.register(YazilarModel, YazilarAdmin)
---
+++ blog/models/yazi.py
@@ -0,0 +1,28 @@
+from enum import unique
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import KategoriModel
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
+class YazilarModel(models.Model):
+
+    resim=models.ImageField(upload_to='yazi_resimleri')
+    baslik = models.CharField(max_length=50)
+    icerik = RichTextField()
+    olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
+    duzenleme_tarihi=models.DateTimeField(auto_now=True)
+    slug= AutoSlugField(populate_from='baslik', unique=True)
+    kategoriler= models.ManyToManyField(KategoriModel, related_name='yazi')
+    ## related_name='yazi' ile ters ilişki kurup kategorideki tüm yazıları görebiliyoruz.
+    yazar= models.ForeignKey(User, on_delete=models.CASCADE, related_name='yazilar')
+    ## on_delete=models.CASCADE burada yazar silinirse yazarın tüm yazılarını siliyoruz.
+
+    class Meta:
+        db_table = 'Yazi'
+        verbose_name_plural ='Yazilar'
+        verbose_name = 'Yazi'
+    
+    def __str__(self):
+        return self.baslik
+
