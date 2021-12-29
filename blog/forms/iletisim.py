4d19277308f606a61c163ccec4774a7fa8a759c3
Ceyda Kamalı <ceydakamali3@gmail.com>
2021-12-29 14:44:39 +0300

    settings file fragmented, email sent

--- blog/forms/iletisim.py
+++ blog/forms/iletisim.py
@@ -1,6 +1,6 @@
 from django import forms
 from blog.models import IletisimModel
-
+from django.core.mail import send_mail
 ##class IletisimForm(forms.Form):
     ##email = forms.EmailField(max_length=100, label='E-mail')
     ##isim_soyisim = forms.CharField(max_length=100, label= 'İsim-Soyisim')
@@ -10,3 +10,12 @@ class IletisimForm(forms.ModelForm):
     class Meta:
         model = IletisimModel
         fields = ('isim_soyisim', 'email', 'mesaj')
+    
+    def send_email(self, mesaj):
+        send_mail(
+            subject='İletişim Formundan Yeni Mesaj Var !',
+            message=mesaj,
+            from_email=None,
+            recipient_list=['ceydakamali3@gmail.com'], ##kime gönderilcek
+            fail_silently=False ##send_emailin hatalarını yakalamak için false yapıyoruz.
+        )
--- config/settings/base.py
+++ config/settings/base.py
@@ -1,21 +1,24 @@
 import os
 from pathlib import Path
+import environ
 
+env= environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fs4-60e5#y%am)#lx%as1d*#)1ga!1+dlasx%ip5hf3pp4l0&h'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+
 
 
 # Application definition
@@ -70,31 +73,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
 # Internationalization
@@ -130,3 +113,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL ='/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL='ceydakamali3@gmail.com'
+EMAIL_HOST_USER = 'ceydakamali3@gmail.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
